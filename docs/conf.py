e22741ab2e1c46b455b1010fa8e16612dc2dd8c2
Andrew Bennecke <jandrew624@hotmail.com>
2026-01-27 15:29:19 -0800

    Sphinx setup

---
+++ docs/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'atlas_free_slice_alignment'
+copyright = '2026, Daniel Tward, Andrew Bennecke, Gary Zhou'
+author = 'Daniel Tward, Andrew Bennecke, Gary Zhou'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+    'sphinxcontrib.htmlhelp',
+    'sphinx.ext.viewcode'
+]
+
+# Jupyter Notebook configurations
+nbsphinx_execute = 'never'
+nbsphinx_execute_arguments = ["--InlineBackend.figure_formats={'svg', 'pdf'}",]
+
+# Configuration for LaTeX output
+latex_documents = [
+    ('index', 'spine_registration.tex', project, author, 'manual'),
+]
+
+# Skip the following packages when building documentation
+autodoc_mock_imports = ['torch','numpy','argparse','matplotlib']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path('..', 'source').resolve()))
+sys.path.insert(0, str(Path('..', 'source', 'scripts').resolve()))
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'bizstyle'
+
+html_static_path = ['docs/_static']
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. atlas_free_slice_alignment documentation master file, created by
+   sphinx-quickstart on Tue Jan 27 15:24:51 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+atlas_free_slice_alignment documentation
+========================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
